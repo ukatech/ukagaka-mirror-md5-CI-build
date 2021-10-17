6a10c51bf3379cf5162738e0dc1f11b91ea880c0
steve green <steve02081504@EDEN.fukwold>
2021-10-17 10:14:32 +0800

    filematch.hpp

---
+++ filematch.hpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <functional>
+
+template<bool default_as_all_match=true>
+class filepathMatcher_t{
+	using namespace std;
+	typedef filepathMatcher_t<default_as_all_match> self_t;
+	vector<wstring> MatchingRules;//*.ai
+	vector<wstring> NotMatchingRules;//!*.inf
+
+	wstring& PreprocessRule(wstring& rule);
+	void AddMatchingRule(wstring rule);
+	void AddNotMatchingRule(wstring rule);
+public:
+	void AddRule(wstring rule);
+	bool IsMatch(wstring s);
+	bool operator()(wstring s){
+		return IsMatch(s);
+	}
+	//ForDir
+private:
+	static filesystem::path base_path;
+	self_t&& GetCopySelfFor(wstring dirlevel);
+	void ForDir_mapper(filesystem::path path, function<void(filesystem::path)>do_what);
+public:
+	void ForDir(filesystem::path Dir, function<void(filesystem::path)>do_what);
+};
+
