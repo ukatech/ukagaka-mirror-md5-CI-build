c0b8e8ac9a02acb3c060adecd69b7049edd8a2ce
steve green <steve02081504@EDEN.fukwold>
2021-10-31 12:52:17 +0800

    Update

--- md5-CI-builder.cpp
+++ md5-CI-builder.cpp
@@ -1,9 +1,35 @@
 #include "my-gists/ukagaka/updates.hpp"
+using namespace std;
 int wmain(int argc,wchar_t** argv){
-	//TESTING
-	updatefile_n::update_file file;
-	file.readrules(L"C:\\Users\\steve02081504\\Documents\\workstation\\Taromati2_workdirs\\ghost-in-the-shell\\md5buildignore.txt");
-	file.read(L"C:\\Users\\steve02081504\\Documents\\workstation\\Taromati2_workdirs\\ghost-in-the-shell\\updates.txt");
-	file.update(L"C:\\Users\\steve02081504\\Documents\\workstation\\Taromati2_workdirs\\ghost-in-the-shell\\");
-	file.write(L"C:\\Users\\steve02081504\\Documents\\workstation\\Taromati2_workdirs\\ghost-in-the-shell\\updates.txt");
+	wstring mode=argv[1];
+	wstring path=argv[2];
+	if(mode==L"ghost_dir"){
+		{
+			updatefile_n::update_file file;
+			file.readrules(path+L"/md5buildignore.txt");
+			file.read(path+L"/updates.txt");
+			file.update(path+L"/");
+			file.write(path+L"/updates.txt");
+			file.write(path+L"/ghost/master/updates.txt");
+		}
+		{
+			path=path+L"/shell";
+			updatefile_n::update_file file;
+			file.readrules(path+L"/md5buildignoreforeach.txt");
+			for(auto& enty : filesystem::directory_iterator(path)){
+				if(is_directory(enty)){
+					file.read(enty.path()/L"updates.txt");
+					file.update(enty.path()/L"");
+					file.write(enty.path()/L"updates.txt");
+				}
+			}
+		}
+	}
+	else if(mode==L"other"){
+		updatefile_n::update_file file;
+		file.readrules(path+L"/md5buildignore.txt");
+		file.read(path+L"/updates.txt");
+		file.update(path);
+		file.write(path+L"/updates.txt");
+	}
 }
